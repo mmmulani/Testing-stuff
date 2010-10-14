042c0f36f83faf05c9b3954d1e06b950f0898642
Frank Yan <studio17@gmail.com>
2010-10-14 12:22:32 -0500

    implemented variable width!

--- slider.js
+++ slider.js
@@ -9,10 +9,18 @@ window.addEventListener('load', function() {
   if (test.value == '50')
     return;
 
-  var slider = document.querySelector('input[type=range]');
-  if (!slider)
+  var sliders = document.querySelectorAll('input[type=range]');
+  if (!sliders.length)
     return;
+
+  Array.prototype.forEach.call(sliders, create)
+}, false);
+
+function create(slider) {
   slider.width = parseInt(getComputedStyle(slider, 0).width);
+  slider.min = '' + slider.getAttribute('min');
+  slider.max = '' + slider.getAttribute('max');
+  slider.step = '' + slider.getAttribute('step');
 
   // XXX stop the focus ring from distorting the box shadow
   slider.onfocus = function() {
@@ -20,7 +28,7 @@ window.addEventListener('load', function() {
   };
 
   slider.adjust = function() {
-    // if invalid, reset min to 0, max to 100, step to 1
+    // if invalid, reset min to 0, max to 100, and/or step to 1
     this.min = isNaN(this.min) ? 0 : +this.min;
     this.max = isNaN(this.max) || this.max < this.min ? 100 : +this.max;
     this.step = isNaN(this.step) ? 1 : +this.step;
@@ -41,14 +49,20 @@ window.addEventListener('load', function() {
     this.adjust();
     // style the control
     var mid = (this.min + this.max) / 2;
-    var dev = this.value < mid ? mid - this.value : this.value - mid;
+    var range = this.max - this.min;
+    var multiplier = this.width / range;
+    var dev = Math.abs(this.value - mid) * multiplier;
     this.style.width = this.width + 2 * dev + 'px';
-    this.style.marginLeft = this.value < mid ? -dev * 2 + 'px' : 0;
-    this.style.marginRight = this.value > mid ? -dev * 2 + 'px' : 0;
-    var shadow = [];
-    for (var i = this.min - this.value; i <= this.max - this.value; i ++)
-      // shadow.push(i + 'px 2px 0 -6px #555');
-      shadow.push(i + 'px 0 0 #444');
+    this.style.marginLeft = (this.value < mid ? -dev * 2 : 0) + 5 + 'px';
+    this.style.marginRight = (this.value > mid ? -dev * 2 : 0) + 5 + 'px';
+    var shadow = [], style = 'px 0 0 #444';
+    // experimental thin style
+    if (~slider.className.split(' ').indexOf('x-thin'))
+      style = 'px 2px 0 -6px #555';
+    var end = (this.max - this.value) * multiplier;
+    for (var i = (this.min - this.value) * multiplier; i < end; i += 4)
+      shadow.push(i + style);
+    shadow.push(end + style);
     this.style.boxShadow = shadow.join();
   };
 
@@ -56,17 +70,19 @@ window.addEventListener('load', function() {
 
   slider.onmousedown = function(e) {
     var mid = (this.min + this.max) / 2;
-    var dev = this.value < mid ? mid - this.value : this.value - mid;
+    var range = this.max - this.min;
+    var multiplier = this.width / range;
+    var dev = Math.abs(this.value - mid) * multiplier;
     var x = e.clientX - this.offsetLeft;
     // distance between click and center of nub
-    var diff = x - dev - this.width / 2;
+    var diff = x - this.width / 2 - dev;
     // whether click was within control bounds
     var valid = this.value < mid ? x > 2 * dev - 5 : x < this.width + 10;
     if (!valid)
       return;
     // if click was not on nub, move nub to click location
     if (diff < -5 || diff > 5) {
-      this.v = this.value - -diff;
+      this.v = this.value - -diff / multiplier;
       this.value = this.v;
       this.draw();
     }
@@ -78,7 +94,7 @@ window.addEventListener('load', function() {
   slider.onmousemove = function(e) {
     if (!this.move)
       return;
-    this.v -= this.x - e.clientX;
+    this.v -= (this.x - e.clientX) * (this.max - this.min) / this.width;
     this.x = e.clientX;
     this.value = this.v;
     this.draw();
@@ -87,4 +103,4 @@ window.addEventListener('load', function() {
   slider.onmouseup = slider.onmouseout = function() {
     this.move = 0;
   };
-}, false);
+}
